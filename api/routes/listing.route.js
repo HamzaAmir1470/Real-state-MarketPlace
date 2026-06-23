bd546d9deb38d7d20b69e6fd74395d058edada9f
HamzaAmir1470 <hamzaamir9733@gmail.com>
2026-06-24 00:20:49 +0500

    Done

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,10 +5,10 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
+router.get('/get', getListings);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
-router.get('/get', getListings);
 
 
 export default router;
